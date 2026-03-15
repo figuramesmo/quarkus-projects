eec9180e51053abff83ee5ee29081b2d63edfa4f
Figurinha <acfigur@gmail.com>
2026-03-15 16:45:37 -0300

    [FIGS] Consumes e Produces Application_JSON

--- project-1/src/main/java/ada/caixa/resources/CourseResource.java
+++ project-1/src/main/java/ada/caixa/resources/CourseResource.java
@@ -4,7 +4,9 @@ import ada.caixa.dto.CourseRequestDTO;
 import ada.caixa.dto.CourseResponseDTO;
 import ada.caixa.service.CourseService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/courses")
@@ -17,8 +19,10 @@ public class CourseResource {
     }
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response createCourse(
-            @Valid CourseRequestDTO courseRequestDTO
+            @Valid @NotNull(message = "O Corpo do seu request não pode ser nulo") CourseRequestDTO courseRequestDTO
             ) {
         CourseResponseDTO response = courseService.createCourse(courseRequestDTO);
         return Response
@@ -28,23 +32,27 @@ public class CourseResource {
     }
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getAllCourses() {
         return Response.ok(courseService.getAllCourses()).build();
     }
 
     @GET
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getCourseById(@PathParam("id") Long id) {
         return Response.ok(courseService.getCourseById(id)).build();
     }
 
     @PUT
     @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response updateCourse(
             @PathParam("id") Long id,
             @QueryParam("update_name") boolean name,
             @QueryParam("update_description") boolean description,
-            @Valid CourseRequestDTO courseRequestDTO
+            @Valid @NotNull(message = "O corpo do seu request não pode ser nulo") CourseRequestDTO courseRequestDTO
     ) {
         CourseResponseDTO response = courseService.updateCourse(id, courseRequestDTO, name, description);
         return Response.ok(response).build();
