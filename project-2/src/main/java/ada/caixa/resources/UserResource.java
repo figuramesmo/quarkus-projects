0be7a8eba3dd542a614cd3682334429ad94b1d34
Figurinha <acfigur@gmail.com>
2026-04-19 20:41:18 -0300

    [FIGS] Get my own credentials

--- project-2/src/main/java/ada/caixa/resources/UserResource.java
+++ project-2/src/main/java/ada/caixa/resources/UserResource.java
@@ -2,13 +2,11 @@ package ada.caixa.resources;
 
 import ada.caixa.dto.UserRequestDTO;
 import ada.caixa.dto.UserResponseDTO;
+import ada.caixa.dto.UserSignInRequestDTO;
 import ada.caixa.service.UserService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -34,5 +32,16 @@ public class UserResource {
                 .status(Response.Status.CREATED)
                 .entity(response)
                 .build();
+    }
+
+    @GET
+    @Path("/me")
+    public Response getMyCredentials(
+            @Valid
+            @NotNull(message = "O Corpo do seu request não pode ser nulo")
+            UserSignInRequestDTO userSignInRequestDTO
+    ){
+        UserResponseDTO response = userService.getUserCredentialsAuthenticated(userSignInRequestDTO);
+        return Response.ok(response).build();
     }
 }
--- project-2/src/main/java/ada/caixa/service/UserService.java
+++ project-2/src/main/java/ada/caixa/service/UserService.java
@@ -2,8 +2,10 @@ package ada.caixa.service;
 
 import ada.caixa.dto.UserRequestDTO;
 import ada.caixa.dto.UserResponseDTO;
+import ada.caixa.dto.UserSignInRequestDTO;
 import ada.caixa.entity.User;
 import ada.caixa.repository.UserRepository;
+import io.quarkus.security.AuthenticationFailedException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
@@ -25,6 +27,23 @@ public class UserService {
         );
 
         userRepository.persist(user);
+
+        return new UserResponseDTO(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole()
+        );
+    }
+
+    @Transactional
+    public UserResponseDTO getUserCredentialsAuthenticated(UserSignInRequestDTO userDTO) {
+        User user = userRepository.findBuSignInCredentialsOptional(
+                userDTO.email(),
+                userDTO.password()
+        ).orElseThrow(
+                () -> new AuthenticationFailedException("Invalid email or password")
+        );
 
         return new UserResponseDTO(
                 user.getId(),
